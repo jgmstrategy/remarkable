ad5ae2db3a9c1137fc75e2e06f48dc1888f485f7
Gideon Tong <gideon@gideontong.com>
2023-04-18 00:14:05 -0700

    Create rent getters and setters

--- remarkable/backend/server/housing/controller/rent.py
+++ remarkable/backend/server/housing/controller/rent.py
@@ -1,3 +1,27 @@
 """Rent controller"""
 
-from remarkable.backend.server.housing.models.rent import api
+from flask_restx import Resource
+
+from remarkable.backend.server.housing.models.rent import entry, api
+
+
+@api.route("/create")
+class CreateEntry(Resource):
+    """Create a rent entry"""
+
+    @api.doc("create_entry")
+    @api.expect(entry)
+    def post(self) -> None:
+        """Create an entry"""
+        api.expect(404)
+
+
+@api.route("/<id>")
+class EntryById(Resource):
+    """Entries as a resource"""
+
+    @api.doc("get_entry_by_id")
+    @api.marshal_with(entry)
+    def get(self, id) -> dict:
+        """Get an entry"""
+        api.abort(404)
