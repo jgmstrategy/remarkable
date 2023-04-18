03bf043c65dfe0431b1599e21eccf3738ff6711d
Gideon Tong <gideon@gideontong.com>
2023-04-18 10:24:52 -0700

    Sale backend code update

--- remarkable/backend/server/housing/controller/sale.py
+++ remarkable/backend/server/housing/controller/sale.py
@@ -2,27 +2,24 @@
 
 from flask_restx import Resource
 
+from remarkable.backend.server.housing.config.sale import listing_params
 from remarkable.backend.server.housing.models.sale import api, listing
 
 
-@api.route("/create")
-class CreateListing(Resource):
-    """Create a sale entry"""
+@api.route("/")
+class ListingById(Resource):
+    """Real estate as a resource"""
+
+    @api.doc("get_listing")
+    @api.expect(listing_params)
+    @api.marshal_with(listing)
+    def get(self) -> dict:
+        """Get a listing"""
+        api.abort(404)
+        return {}
 
     @api.doc("create_listing")
     @api.expect(listing)
     def post(self) -> None:
         """Create a listing"""
         api.abort(404)
-
-
-@api.route("/id/<id>")
-class ListingById(Resource):
-    """Real estate as a resource"""
-
-    @api.doc("get_listing")
-    @api.marshal_with(listing)
-    def get(self) -> dict:
-        """Get a listing"""
-        api.abort(404)
-        return {}
