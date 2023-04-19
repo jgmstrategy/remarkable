ec5b6e8161675da1332a22b70c69e302cedb1655
Gideon Tong <gideon@gideontong.com>
2023-04-18 17:26:23 -0700

    Add ESLint GitHub action

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,25 +1,14 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload, Outlet } from '@remix-run/react';
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta charSet='utf-8' />
+        <title>Remix Server</title>
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
